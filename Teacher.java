fbdf9e65c79fd18a6fafe96ec9e3b9b407714733
dylanb-101 <91355772+dylanb-101@users.noreply.github.com>
2025-01-21 13:20:35 -0500

    Add files via upload

--- Teacher.java
+++ Teacher.java
@@ -163,6 +163,36 @@ public class Teacher implements Comparable {
 
     }
 
+    public int getAdjustedSocialCredit()
+    {
+        int updatedSocialCredit = socialCredit;
+
+        for(int i = 0; i < this.getDuties().size(); i++)
+        {
+
+            Duty nextDuty = this.getDuties().get(i);
+
+            if(nextDuty.getPeriod().getValue() == 1 || (nextDuty.getPeriod().getValue() == 8 && nextDuty.getDay().equals("M"))
+                    || (nextDuty.getPeriod().getValue() == 7 && !nextDuty.getDay().equals("M")))
+                updatedSocialCredit -= 2;
+
+            if(nextDuty.getType().equals(DutyType.FROSH_PASCACK))
+                updatedSocialCredit -= 10;
+
+            if(nextDuty.getType().equals(DutyType.COVERAGE))
+            {
+                if(nextDuty.getPeriod().getValue() == 1 || (nextDuty.getPeriod().getValue() == 8 && nextDuty.getDay().equals("M"))
+                        || (nextDuty.getPeriod().getValue() == 7 && !nextDuty.getDay().equals("M")))
+                    updatedSocialCredit -= 2;
+            }
+
+            if(nextDuty.getType().equals(DutyType.HALL))
+                updatedSocialCredit += 1;
+        }
+
+        return updatedSocialCredit;
+    }
+
     public void fillInLunchesAndFrees() {
         String[] days = {"M", "T", "W", "R", "F"};
 
@@ -475,7 +505,7 @@ public class Teacher implements Comparable {
 
     private int rankAssignment(Assignment assignment) {
 
-        int score = adjustForSocialCredit();
+        int score = getAdjustedSocialCredit();
 
         if(assignment instanceof Course) return -2900000;
 
